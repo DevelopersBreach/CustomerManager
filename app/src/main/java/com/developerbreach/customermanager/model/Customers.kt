a75780b9a22cfea8d1a3f66a51b582db1a1304e7
Rajasekhar <rajashekarraju123@gmail.com>
2020-08-10 23:34:34 +0530

    More properties included to model Customers.kt, will be added to firestore

--- app/src/main/java/com/developerbreach/customermanager/model/Customers.kt
+++ app/src/main/java/com/developerbreach/customermanager/model/Customers.kt
@@ -8,17 +8,35 @@ class Customers {
 
     private var baseId: Int = 0
     var billNumber: Int = 0
+    var totalItems = 0
+    lateinit var itemType: String
     lateinit var name: String
+    lateinit var email: String
     lateinit var contact: String
+    var status: Boolean = false
     lateinit var date: String
 
     constructor() {}
 
-    constructor(baseId: Int, billNumber: Int, name: String, contact: String, date: String) {
+    constructor(
+        baseId: Int,
+        billNumber: Int,
+        totalItems: Int,
+        itemType: String,
+        name: String,
+        email: String,
+        contact: String,
+        status: Boolean,
+        date: String
+    ) {
         this.baseId = baseId
         this.billNumber = billNumber
+        this.totalItems = totalItems
+        this.itemType = itemType
         this.name = name
+        this.email = email
         this.contact = contact
+        this.status = status
         this.date = date
     }
 }

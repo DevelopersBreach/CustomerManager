c0c385e0d1c98937e06c5511a087db9b16dfc221
Rajasekhar <rajashekarraju123@gmail.com>
2020-09-06 00:05:59 +0530

    Moved text fields validation, buttons and firestore operation to binding class

--- app/src/main/java/com/developerbreach/customermanager/view/editor/EditorBindingAdapter.kt
+++ app/src/main/java/com/developerbreach/customermanager/view/editor/EditorBindingAdapter.kt
@@ -1,13 +1,24 @@
 package com.developerbreach.customermanager.view.editor
 
 
+import android.app.DatePickerDialog
+import android.content.Context
 import android.util.Patterns
 import android.view.View
-import android.widget.Button
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.BindingAdapter
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
 import com.developerbreach.customermanager.R
+import com.developerbreach.customermanager.model.Customers
+import com.developerbreach.customermanager.utils.isNetworkConnected
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
 import com.google.common.base.Strings
+import java.text.DateFormat
+import java.util.*
 
 
 @BindingAdapter(
@@ -18,9 +29,8 @@ fun Button.setValidateButton(
     billNumberEditText: TextInputEditText,
     contactEditText: TextInputEditText,
     mailEditText: TextInputEditText,
-    submitButton: Button
+    submitButton: Button,
 ) {
-
     this.setOnClickListener {
         val length = billNumberEditText.text!!.length
         val validContact = contactEditText.text!!.length
@@ -45,5 +55,164 @@ fun Button.setValidateButton(
             this.visibility = View.VISIBLE
             submitButton.visibility = View.GONE
         }
+    }
+}
+
+
+@BindingAdapter(
+    "bindSubmitButton", "bindBillNumberEditSubmission", "bindNumOfItemsEditSubmission",
+    "bindCustomerNameEditSubmission", "bindMailEditSubmission", "bindContactEditSubmission",
+    "bindItemTypeDropDownSubmission", "bindCurrentDateTextSubmission",
+    "bindDeliveryDateTextSubmission"
+)
+fun Button.setSubmitButton(
+    viewModel: EditorViewModel,
+    billNumberEditText: TextInputEditText,
+    numberOfItemsEditText: TextInputEditText,
+    customerNameEditText: TextInputEditText,
+    mailEditText: TextInputEditText,
+    contactEditText: TextInputEditText,
+    itemTypeDropDown: AutoCompleteTextView,
+    currentDate: TextView,
+    deliveryDate: TextView,
+) {
+    this.setOnClickListener {
+
+        val customers = Customers(
+            billNumberEditText.text.toString(),
+            numberOfItemsEditText.text.toString(),
+            itemTypeDropDown.text.toString(),
+            customerNameEditText.text.toString(),
+            mailEditText.text.toString(),
+            contactEditText.text.toString(),
+            false,
+            currentDate.text.toString(),
+            deliveryDate.text.toString()
+        )
+
+        if (isNetworkConnected(context)) {
+            viewModel.collection.document(billNumberEditText.text.toString()).set(customers)
+                .addOnSuccessListener {
+                    firestoreSuccessListener(
+                        customerNameEditText.text.toString(),
+                        billNumberEditText.text.toString()
+                    )
+                }
+                .addOnFailureListener { firestoreFailureListener() }
+        } else {
+            showNetworkDialog(context)
+        }
+    }
+}
+
+fun showNetworkDialog(context: Context) {
+    MaterialAlertDialogBuilder(context, R.style.Widget_Customer_Dialog)
+        .setTitle(context.getString(R.string.dialog_error_title_text))
+        .setMessage(context.getString(R.string.dialog_error_message_text))
+        .setPositiveButton(context.getString(R.string.dialog_positive_button_ok)) { dialog, _ ->
+            dialog.dismiss()
+        }
+        .show()
+}
+
+private fun Button.firestoreSuccessListener(
+    customerName: String,
+    billNumber: String,
+) {
+    MaterialAlertDialogBuilder(context, R.style.Widget_Customer_Dialog)
+        .setTitle("$customerName - $billNumber")
+        .setMessage(context.getString(R.string.dialog_positive_message_text))
+        .setPositiveButton(context.getString(R.string.dialog_positive_button_ok)) { dialog, _ ->
+            dialog.dismiss()
+            findNavController().navigateUp()
+        }
+        .show()
+}
+
+private fun Button.firestoreFailureListener() {
+    val message = context.getString(R.string.customer_firestore_unsuccessful)
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+
+@BindingAdapter("bindCurrentDateTextView")
+fun TextView.setCurrentDateTextView(
+    viewModel: EditorViewModel,
+) {
+    val dayOfMonth = viewModel.date
+    val month = viewModel.month
+    val year = viewModel.year
+    val currentDate = "$dayOfMonth/${month + 1}/$year"
+    this.text = currentDate
+}
+
+
+@BindingAdapter("bindDeliveryDateTextView")
+fun TextView.setDeliveryDateTextView(
+    viewModel: EditorViewModel,
+) {
+    val dayOfMonth = viewModel.date
+    val month = viewModel.month
+    val year = viewModel.year
+    val deliveryDate = "${dayOfMonth + 1}/${month + 1}/$year"
+    this.text = deliveryDate
+
+    this.setOnClickListener {
+        val datePickerDialog = DatePickerDialog(context, { _, i, i2, i3 ->
+            onDateSet(i, i2, i3, viewModel, this)
+        }, year, month, dayOfMonth)
+        datePickerDialog.show()
+    }
+}
+
+fun onDateSet(
+    year: Int,
+    month: Int,
+    dayOfMonth: Int,
+    viewModel: EditorViewModel,
+    textView: TextView,
+) {
+    viewModel.calendar.set(Calendar.YEAR, year)
+    viewModel.calendar.set(Calendar.MONTH, month)
+    viewModel.calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+    val format =
+        DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(viewModel.calendar.time)
+    textView.text = format
+}
+
+
+@BindingAdapter("bindItemTypeDropDown")
+fun AutoCompleteTextView.setItemTypeDropDown(
+    context: Context,
+) {
+    val adapter: ArrayAdapter<String> = ArrayAdapter(
+        context,
+        R.layout.drop_down_item,
+        resources.getStringArray(R.array.dropdown_content)
+    )
+
+    this.setAdapter(adapter)
+    this.setText(adapter.getItem(0), false)
+    this.setOnItemClickListener { _, _, itemId, _ ->
+        when (itemId) {
+            0 -> context.getString(R.string.item_type_dress)
+            1 -> context.getString(R.string.item_type_blouse)
+            2 -> context.getString(R.string.item_type_ghagra)
+            3 -> context.getString(R.string.item_type_lehenga)
+            4 -> context.getString(R.string.item_type_long_frock)
+        }
+    }
+}
+
+
+@BindingAdapter("bindEditorFragmentNavigationIcon", "bindEditorFragmentTitle")
+fun Toolbar.setEditorFragmentNavigationIcon(
+    activity: FragmentActivity,
+    toolbarTitleArgs: String,
+) {
+    ((activity) as AppCompatActivity).setSupportActionBar(this)
+    this.title = toolbarTitleArgs
+    this.setNavigationOnClickListener {
+        findNavController().navigateUp()
     }
 }
--- app/src/main/java/com/developerbreach/customermanager/view/editor/EditorFragment.kt
+++ app/src/main/java/com/developerbreach/customermanager/view/editor/EditorFragment.kt
@@ -1,57 +1,30 @@
 package com.developerbreach.customermanager.view.editor
 
-import android.app.DatePickerDialog
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.*
-import android.widget.ArrayAdapter
-import android.widget.DatePicker
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.developerbreach.customermanager.R
 import com.developerbreach.customermanager.databinding.FragmentEditorBinding
 import com.developerbreach.customermanager.model.Customers
-import com.developerbreach.customermanager.utils.AppTextWatcher
-import com.developerbreach.customermanager.utils.COLLECTION_PATH
-import com.developerbreach.customermanager.utils.isNetworkConnected
-import com.developerbreach.customermanager.viewModel.EditorViewModel
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.firestore.FirebaseFirestore
-import java.text.DateFormat
-import java.util.*
 
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class EditorFragment : Fragment(), DatePickerDialog.OnDateSetListener {
+class EditorFragment : Fragment() {
 
-    private val viewModel: EditorViewModel by viewModels()
     private lateinit var binding: FragmentEditorBinding
-    private lateinit var firestore: FirebaseFirestore
+    private val viewModel: EditorViewModel by viewModels()
     private var customers: Customers? = null
     private lateinit var toolbarTitleArgs: String
 
-    private lateinit var billNumber: String
-    private lateinit var numOfItems: String
-    private lateinit var customerName: String
-    private lateinit var dropDownSelection: String
-    private lateinit var currentDate: String
-    private lateinit var deliveryDate: String
-    private lateinit var mail: String
-    private lateinit var contact: String
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Access a Cloud Firestore instance from your Activity
-        firestore = FirebaseFirestore.getInstance()
 
-        if (!isNetworkConnected(requireContext())) {
-            showNoNetworkDialog()
+        if (!viewModel.isConnected) {
+            showNetworkDialog(requireContext())
         }
 
         customers = EditorFragmentArgs.fromBundle(requireArguments()).editorFragmentArgs
@@ -65,47 +38,64 @@ class EditorFragment : Fragment(), DatePickerDialog.OnDateSetListener {
         // Inflate the layout for this fragment
         binding = FragmentEditorBinding.inflate(inflater, container, false)
         binding.viewModel = viewModel
-        binding.executePendingBindings()
+        binding.context = requireContext()
+        binding.toolbarTitleArgs = toolbarTitleArgs
+        binding.activity = requireActivity()
         binding.lifecycleOwner = this
+        binding.executePendingBindings()
         setHasOptionsMenu(true)
-        setFragmentToolbar()
         return binding.root
-    }
-
-    private fun setFragmentToolbar() {
-        ((activity) as AppCompatActivity).setSupportActionBar(binding.toolbarFragmentEditor)
-        binding.toolbarFragmentEditor.title = toolbarTitleArgs
-        binding.toolbarFragmentEditor.setNavigationOnClickListener {
-            findNavController().navigateUp()
-        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        setCurrentDate()
-        setItemDropDownType()
-
-        binding.billNumberEditText.addTextChangedListener(watchBillNumber())
-        binding.numberOfItemsEditText.addTextChangedListener(watchNumOfItems())
-        binding.customerNameEditText.addTextChangedListener(watchCustomerName())
-        binding.mailEditText.addTextChangedListener(watchMail())
-        binding.contactEditText.addTextChangedListener(watchContact())
+        setTextChangeListeners()
         performNullOrEmptyCheck()
         addDataToFields()
-
-        binding.submitButton.setOnClickListener(firestoreSubmitListener())
     }
 
-    private fun addDataToFields() {
-        if (customers != null) {
-            binding.billNumberEditText.setText(customers!!.billNumber)
-            binding.numberOfItemsEditText.setText(customers!!.totalItems)
-            binding.itemTypeDropDown.setText(customers!!.itemType)
-            binding.customerNameEditText.setText(customers!!.name)
-            binding.contactEditText.setText(customers!!.contact)
-            binding.mailEditText.setText(customers!!.email)
-            binding.dateDisplayTextView.text = customers!!.date
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_clear -> {
+                binding.billNumberEditText.setText("")
+                binding.numberOfItemsEditText.setText("")
+                binding.customerNameEditText.setText("")
+                binding.mailEditText.setText("")
+                binding.contactEditText.setText("")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setTextChangeListeners() {
+        binding.billNumberEditText.doAfterTextChanged { query ->
+            viewModel.validateField(query.toString(), binding.billNumberInput)
+            performNullOrEmptyCheck()
+        }
+
+        binding.numberOfItemsEditText.doAfterTextChanged { query ->
+            viewModel.validateField(query.toString(), binding.numberOfItemsInput)
+            performNullOrEmptyCheck()
+        }
+
+        binding.customerNameEditText.doAfterTextChanged { query ->
+            viewModel.validateField(query.toString(), binding.customerNameInput)
+            performNullOrEmptyCheck()
+        }
+
+        binding.mailEditText.doAfterTextChanged { query ->
+            viewModel.isValidEmail(query, binding.customerMailInput)
+            performNullOrEmptyCheck()
+        }
+
+        binding.contactEditText.doAfterTextChanged { query ->
+            viewModel.isValidContact(query, binding.customerContactInput)
+            performNullOrEmptyCheck()
         }
     }
 
@@ -122,173 +112,16 @@ class EditorFragment : Fragment(), DatePickerDialog.OnDateSetListener {
         }
     }
 
-    private fun setCurrentDate() {
-
-        val dayOfMonth = viewModel.date
-        val month = viewModel.month
-        val year = viewModel.year
-
-        val currentDate = "$dayOfMonth/$month/$year"
-        binding.dateDisplayTextView.text = currentDate
-
-        deliveryDate = "${dayOfMonth + 1}/$month/$year"
-        binding.dateDeliveryTextView.text = deliveryDate
-
-        binding.dateDeliveryTextView.setOnClickListener {
-            val datePickerDialog = DatePickerDialog(requireContext(), { view, i, i2, i3 ->
-                onDateSet(view, i, i2, i3)
-            }, year, month, dayOfMonth)
-            datePickerDialog.show()
-        }
-
-        binding.dateDisplayTextView.setOnClickListener {
-            val datePickerDialog = DatePickerDialog(requireContext(), { view, i, i2, i3 ->
-                onDateSet(view, i, i2, i3)
-            }, year, month, dayOfMonth)
-            datePickerDialog.show()
-        }
-    }
-
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        viewModel.calendar.set(Calendar.YEAR, year)
-        viewModel.calendar.set(Calendar.MONTH, month)
-        viewModel.calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-        val format = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(viewModel.calendar.time)
-        binding.dateDeliveryTextView.text = format
-    }
-
-    private fun setItemDropDownType() {
-        val adapter: ArrayAdapter<String> = ArrayAdapter(
-            requireContext(),
-            R.layout.drop_down_item,
-            resources.getStringArray(R.array.dropdown_content)
-        )
-
-        binding.itemTypeDropDown.setAdapter(adapter)
-        binding.itemTypeDropDown.setText(adapter.getItem(0), false)
-        binding.itemTypeDropDown.setOnItemClickListener { _, _, itemId, _ ->
-            when (itemId) {
-                0 -> getString(R.string.item_type_dress)
-                1 -> getString(R.string.item_type_blouse)
-                2 -> getString(R.string.item_type_ghagra)
-                3 -> getString(R.string.item_type_lehenga)
-                4 -> getString(R.string.item_type_long_frock)
-            }
-        }
-
-        dropDownSelection = binding.itemTypeDropDown.text.toString()
-    }
-
-    private fun firestoreSubmitListener(): (View) -> Unit = {
-        val customers = Customers(
-            billNumber,
-            numOfItems,
-            dropDownSelection,
-            customerName,
-            mail,
-            contact,
-            false,
-            currentDate,
-            deliveryDate
-        )
-
-        if (isNetworkConnected(requireContext())) {
-            val collection = firestore.collection(COLLECTION_PATH)
-            collection.document(billNumber).set(customers)
-                .addOnSuccessListener { firestoreSuccessListener(customerName, billNumber) }
-                .addOnFailureListener { firestoreFailureListener() }
-        } else {
-            showNoNetworkDialog()
-        }
-    }
-
-    private fun showNoNetworkDialog() {
-        MaterialAlertDialogBuilder(requireContext(), R.style.Widget_Customer_Dialog)
-            .setTitle(getString(R.string.dialog_error_title_text))
-            .setMessage(getString(R.string.dialog_error_message_text))
-            .setPositiveButton(getString(R.string.dialog_positive_button_ok)) { dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
-    }
-
-    private fun firestoreSuccessListener(
-        customerName: String,
-        billNumber: String,
-    ) {
-        MaterialAlertDialogBuilder(requireContext(), R.style.Widget_Customer_Dialog)
-            .setTitle("$customerName - $billNumber")
-            .setMessage(getString(R.string.dialog_positive_message_text))
-            .setPositiveButton(getString(R.string.dialog_positive_button_ok)) { dialog, _ ->
-                dialog.dismiss()
-                findNavController().navigateUp()
-            }
-            .show()
-    }
-
-    private fun firestoreFailureListener() {
-        Snackbar.make(
-            requireView(),
-            getString(R.string.customer_firestore_unsuccessful),
-            Snackbar.LENGTH_SHORT
-        ).show()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_main, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        when (item.itemId) {
-            R.id.action_clear -> {
-                binding.billNumberEditText.setText("")
-                binding.numberOfItemsEditText.setText("")
-                binding.customerNameEditText.setText("")
-                binding.mailEditText.setText("")
-                binding.contactEditText.setText("")
-            }
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun watchBillNumber(): TextWatcher = object : AppTextWatcher() {
-        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            billNumber =
-                viewModel.validateCustomerDetails(text.toString(), binding.billNumberInput)
-            performNullOrEmptyCheck()
-        }
-    }
-
-    private fun watchNumOfItems(): TextWatcher = object : AppTextWatcher() {
-        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            numOfItems =
-                viewModel.validateCustomerDetails(text.toString(), binding.numberOfItemsInput)
-            performNullOrEmptyCheck()
-        }
-    }
-
-    private fun watchCustomerName(): TextWatcher = object : AppTextWatcher() {
-        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            customerName =
-                viewModel.validateCustomerDetails(text.toString(), binding.customerNameInput)
-            performNullOrEmptyCheck()
-        }
-    }
-
-    private fun watchMail(): TextWatcher = object : AppTextWatcher() {
-        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            mail = viewModel.isValidEmail(text as Editable?, binding.customerMailInput)
-            performNullOrEmptyCheck()
-        }
-    }
-
-    private fun watchContact(): TextWatcher = object : AppTextWatcher() {
-        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            contact = viewModel.isValidContact(text as Editable?, binding.customerContactInput)
-            performNullOrEmptyCheck()
+    private fun addDataToFields() {
+        if (customers != null) {
+            binding.billNumberEditText.setText(customers!!.billNumber)
+            binding.numberOfItemsEditText.setText(customers!!.totalItems)
+            binding.itemTypeDropDown.setText(customers!!.itemType)
+            binding.customerNameEditText.setText(customers!!.name)
+            binding.contactEditText.setText(customers!!.contact)
+            binding.mailEditText.setText(customers!!.email)
+            binding.dateDisplayTextView.text = customers!!.date
+            binding.dateDeliveryTextView.text = customers!!.delivery
         }
     }
 }

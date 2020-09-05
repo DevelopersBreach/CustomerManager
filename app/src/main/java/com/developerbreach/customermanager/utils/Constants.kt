8cb31db5d3d25c3e7fbc1467c497120a23f81bbc
Rajasekhar <rajashekarraju123@gmail.com>
2020-09-05 23:59:33 +0530

    Added firebase auth providers to constants

--- app/src/main/java/com/developerbreach/customermanager/utils/Constants.kt
+++ app/src/main/java/com/developerbreach/customermanager/utils/Constants.kt
@@ -1,5 +1,14 @@
 package com.developerbreach.customermanager.utils
 
+import com.firebase.ui.auth.AuthUI
+
 
 const val COLLECTION_PATH: String = "customers"
 const val COLLECTION_PATH_FIELD_STATUS: String = "status"
+
+
+const val SIGN_IN_REQUEST_CODE = 9001
+val PROVIDERS = arrayListOf(
+    AuthUI.IdpConfig.GoogleBuilder().build(),
+    AuthUI.IdpConfig.EmailBuilder().build()
+)

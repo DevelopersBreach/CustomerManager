bb0ec4f13761ea8b2e7532384037e9e3dc24b84f
Rajasekhar <rajashekarraju123@gmail.com>
2020-09-06 00:01:26 +0530

    Renamed and moved MainFragment.kt to DashboardFragment.kt to new package

---
+++ app/src/main/java/com/developerbreach/customermanager/view/dashboard/DashboardFragment.kt
@@ -0,0 +1,47 @@
+package com.developerbreach.customermanager.view.dashboard
+
+import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.developerbreach.customermanager.R
+import com.developerbreach.customermanager.auth.AuthenticationState
+import com.developerbreach.customermanager.databinding.FragmentDashboardBinding
+import com.google.firebase.auth.FirebaseAuth
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class DashboardFragment : Fragment() {
+
+    private lateinit var binding: FragmentDashboardBinding
+    private val viewModel: DashboardViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        binding.navController = findNavController()
+        binding.lifecycleOwner = this
+        binding.executePendingBindings()
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.authenticationState.observe(viewLifecycleOwner, { authState ->
+            if (authState == AuthenticationState.AUTHENTICATED) {
+                val googleUser = FirebaseAuth.getInstance().currentUser!!
+                Toast.makeText(context, googleUser.displayName, Toast.LENGTH_SHORT).show()
+            } else if (authState == AuthenticationState.UNAUTHENTICATED) {
+                findNavController().navigate(
+                    R.id.dashboardToLoginFragment
+                )
+            }
+        })
+    }
+}

bf133b14088cb574173ae00f17d33dbeac913817
Rajasekhar <rajashekarraju123@gmail.com>
2020-09-08 00:00:04 +0530

    Declared constants which are used across all files to know status replacing type boolean

--- app/src/main/java/com/developerbreach/customermanager/utils/Constants.kt
+++ app/src/main/java/com/developerbreach/customermanager/utils/Constants.kt
@@ -6,6 +6,8 @@ import com.firebase.ui.auth.AuthUI
 const val COLLECTION_PATH: String = "customers"
 const val COLLECTION_PATH_FIELD_STATUS: String = "status"
 
+const val DELIVERY_STATUS_PENDING = 0
+const val DELIVERY_STATUS_COMPLETED = 1
 
 const val SIGN_IN_REQUEST_CODE = 9001
 val PROVIDERS = arrayListOf(
